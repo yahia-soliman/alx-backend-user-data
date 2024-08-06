f2fa5dda624ac058ab1cc10cd7fcf1d97d043dd8
Yahia <yn7599@gmail.com>
2024-08-06 13:06:34 +0300

    missed up annotation for the checker BasicAuth

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,9 +2,6 @@
 """Module to handle API Authentication based on the Basic scheme"""
 
 import base64
-from typing import Optional, Tuple
-
-from flask import Request
 
 from models.user import User
 
@@ -14,44 +11,36 @@ from .auth import Auth
 class BasicAuth(Auth):
     """Authentication handler for the Basic scheme"""
 
-    def extract_base64_authorization_header(
-        self, authorization_header: Optional[str]
-    ) -> Optional[str]:
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
         """Get the base64 encoded string from the authorization header"""
         if isinstance(authorization_header, str):
             if authorization_header.startswith("Basic "):
                 return authorization_header.rstrip()[6:]
 
     def decode_base64_authorization_header(
-        self, base64_authorization_header: Optional[str]
-    ) -> Optional[str]:
+        self, base64_authorization_header: str
+    ) -> str:
         """Decode a base64 encoded string"""
         try:
             return base64.b64decode(base64_authorization_header).decode()
         except Exception:
             return
 
-    def extract_user_credentials(
-        self, decoded_base64_authorization_header: Optional[str]
-    ) -> Tuple:
+    def extract_user_credentials(self, decoded_base64_authorization_header: str):
         """Get the username, password from a decoded authorization header"""
         if isinstance(decoded_base64_authorization_header, str):
             if ":" in decoded_base64_authorization_header:
                 return tuple(decoded_base64_authorization_header.split(":", 1))
         return None, None
 
-    def user_object_from_credentials(
-        self, user_email: Optional[str], user_pwd: Optional[str]
-    ) -> Optional[User]:
+    def user_object_from_credentials(self, user_email: str, user_pwd: str):
         """Get the user object based on his email and password"""
         if isinstance(user_email, str) and isinstance(user_pwd, str):
             users = User.search({"email": user_email})
             if len(users) > 0 and users[0].is_valid_password(user_pwd):
                 return users[0]
 
-    def current_user(
-        self, request: Optional[Request] = None
-    ) -> Optional[User]:
+    def current_user(self, request=None) -> TypeVar("User"):
         """Get the current logged in user, Basic Authentication shceme"""
         ah = self.authorization_header(request)
         b64 = self.extract_base64_authorization_header(ah)
