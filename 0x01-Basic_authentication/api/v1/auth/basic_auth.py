4f0103164a3dba1deb1e35169309ab9da17a8c49
Yahia <yn7599@gmail.com>
2024-08-06 13:13:07 +0300

    Basic - Other methods

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 """Handling API Authentication based on the Basic scheme"""
 
+import base64
+
+from models.user import User
 
 from .auth import Auth
 
@@ -13,3 +16,32 @@ class BasicAuth(Auth):
         if isinstance(authorization_header, str):
             if authorization_header.startswith("Basic "):
                 return authorization_header.rstrip()[6:]
+
+    def decode_base64_authorization_header(self, base64_authorization_header: str) -> str:  # nopep8
+        """Decode a base64 encoded string"""
+        try:
+            return base64.b64decode(base64_authorization_header).decode()
+        except Exception:
+            return
+
+    def extract_user_credentials(self, decoded_base64_authorization_header: str):  # nopep8
+        """Get the username, password from a decoded authorization header"""
+        if isinstance(decoded_base64_authorization_header, str):
+            if ":" in decoded_base64_authorization_header:
+                return tuple(decoded_base64_authorization_header.split(":", 1))
+        return None, None
+
+    def user_object_from_credentials(self, user_email: str, user_pwd: str):
+        """Get the user object based on his email and password"""
+        if isinstance(user_email, str) and isinstance(user_pwd, str):
+            users = User.search({"email": user_email})
+            if len(users) > 0 and users[0].is_valid_password(user_pwd):
+                return users[0]
+
+    def current_user(self, request=None) -> TypeVar("User"):
+        """Get the current logged in user, Basic Authentication shceme"""
+        ah = self.authorization_header(request)
+        b64 = self.extract_base64_authorization_header(ah)
+        b64_decoded = self.decode_base64_authorization_header(b64)
+        email, pwd = self.extract_user_credentials(b64_decoded)
+        return self.user_object_from_credentials(email, pwd)
