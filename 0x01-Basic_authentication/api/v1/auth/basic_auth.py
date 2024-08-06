a108aba2f87a1280515c1cc35455fe045f301e84
Yahia <yn7599@gmail.com>
2024-08-06 13:11:46 +0300

    Basic - Base64 part

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python3
 """Handling API Authentication based on the Basic scheme"""
 
+
 from .auth import Auth
 
 
 class BasicAuth(Auth):
     """Authentication handler for the Basic scheme"""
 
-    pass
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:  # nopep8
+        """Get the base64 encoded string from the authorization header"""
+        if isinstance(authorization_header, str):
+            if authorization_header.startswith("Basic "):
+                return authorization_header.rstrip()[6:]
