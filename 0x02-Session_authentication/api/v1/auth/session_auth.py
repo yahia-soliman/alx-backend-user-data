24a1f7ae42199b29b5b0c18f4e454ff4813ad7f2
Yahia <yn7599@gmail.com>
2024-08-07 14:04:47 +0300

    retrieve session

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """Handling API Authentication based on the Basic scheme"""
 
+from typing import Dict
 from uuid import uuid4
 
 from .auth import Auth
@@ -9,7 +10,7 @@ from .auth import Auth
 class SessionAuth(Auth):
     """Authentication handler for the Basic scheme"""
 
-    user_id_by_session_id = {}
+    user_id_by_session_id: Dict[str, str] = {}
 
     def create_session(self, user_id: str = None) -> str:
         """Create a new session id"""
@@ -17,3 +18,7 @@ class SessionAuth(Auth):
             s_id = str(uuid4())
             self.user_id_by_session_id[s_id] = user_id
             return s_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """Get the user's id based on session id"""
+        return self.user_id_by_session_id.get(session_id)
