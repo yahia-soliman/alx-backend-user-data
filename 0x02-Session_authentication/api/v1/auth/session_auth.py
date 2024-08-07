21d6bceb5d1f01df55323f1d28b2d33252891c85
Yahia <yn7599@gmail.com>
2024-08-07 13:59:11 +0300

    session creation

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,10 +1,19 @@
 #!/usr/bin/env python3
 """Handling API Authentication based on the Basic scheme"""
 
+from uuid import uuid4
+
 from .auth import Auth
 
 
 class SessionAuth(Auth):
     """Authentication handler for the Basic scheme"""
 
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """Create a new session id"""
+        if isinstance(user_id, str):
+            s_id = str(uuid4())
+            self.user_id_by_session_id[s_id] = user_id
+            return s_id
