77b625ea9881c2a74b518129abc8cd178306f35f
Yahia <yn7599@gmail.com>
2024-08-07 18:39:33 +0300

    Session Auth with Database

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -31,7 +31,10 @@ elif AUTH_TYPE == "session_exp_auth":
     from api.v1.auth.session_exp_auth import SessionExpAuth
 
     auth = SessionExpAuth()
+elif AUTH_TYPE == "session_db_auth":
+    from api.v1.auth.session_db_auth import SessionDBAuth
 
+    auth = SessionDBAuth()
 
 
 @app.before_request
---
+++ 0x02-Session_authentication/api/v1/auth/session_db_auth.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+"""Handling API Authentication
+
+Authentication method: session cookie with expiration
+"""
+
+from datetime import datetime, timedelta
+from uuid import uuid4
+
+from models.user_session import UserSession
+
+from .session_exp_auth import SessionExpAuth
+
+
+class SessionDBAuth(SessionExpAuth):
+    """Session Auth with Expiration"""
+
+    def create_session(self, user_id: str = None) -> str:
+        """Create a new login session"""
+        session = UserSession(user_id=user_id, session_id=str(uuid4()))
+        session.save()
+        return session.session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """Get user id based on session_id"""
+        session = UserSession.search({"session_id": session_id})
+        if len(session) == 0:
+            return
+        session = session[0]
+        user_id = session.user_id
+        if self.session_duration <= 0:
+            return user_id
+        if session.created_at:
+            diff: timedelta = datetime.utcnow() - session.created_at
+            if diff.total_seconds() > self.session_duration:
+                session.remove()
+            else:
+                return session.user_id
+
+    def destroy_session(self, request=None):
+        """Delete the current session and logout"""
+        session_id = self.session_cookie(request)
+        if session_id:
+            session = UserSession.search({"session_id": session_id})
+            if len(session) > 0:
+                session[0].remove()
+                return True
+        return False
