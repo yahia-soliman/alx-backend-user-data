186cf96e8c3d591164f68ad01b679d648dd64551
Yahia <yn7599@gmail.com>
2024-08-07 13:51:29 +0300

    session_auth init

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Handling API Authentication based on the Basic scheme"""
+
+from .auth import Auth
+
+
+class SessionAuth(Auth):
+    """Authentication handler for the Basic scheme"""
+
+    pass
