44c4a9ae23213fef2885855036494de1c11c8384
Yahia <yn7599@gmail.com>
2024-08-15 00:38:30 +0300

    DELETE /sessions route

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Flask app for the authentication service"""
 
-from flask import Flask, abort, jsonify, request
+from flask import Flask, abort, jsonify, redirect, request, url_for
 
 from auth import Auth
 
@@ -41,6 +41,18 @@ def login():
     res = jsonify({"email": email, "message": "logged in"})
     res.set_cookie("session_id", session_id)
     return res if session_id else abort(401)
+
+
+@app.route("/sessions", methods=["DELETE"])
+def logout():
+    """Logout and delete session"""
+    session_id = request.cookies.get("session_id")
+    user = AUTH.get_user_from_session_id(session_id)
+    if user:
+        AUTH.destroy_session(user.id)
+        return redirect("/")
+    else:
+        return abort(403)
 
 
 if __name__ == "__main__":
