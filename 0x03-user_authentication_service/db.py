51138f16bc3bbc72f27ba4e458e4d1f11daa5afb
Yahia <yn7599@gmail.com>
2024-08-14 13:49:16 +0300

    no echo

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -9,11 +9,11 @@ from user import Base, User
 
 
 class DB:
-    """DB class"""
+    """DB connection class"""
 
     def __init__(self) -> None:
         """Initialize a new DB instance"""
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        self._engine = create_engine("sqlite:///a.db")
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
