7ab92caf7eb50dee2b813fcf52fffe4c06dfc730
Yahia <yn7599@gmail.com>
2024-08-14 12:44:26 +0300

    0x03: User Auth init

---
+++ 0x03-user_authentication_service/README.md
@@ -0,0 +1,7 @@
+In the industry, you should not implement your own authentication system and use a module or framework that doing it for you (like in Python-Flask: Flask-User). Here, for the learning purpose, we will walk through each step of this mechanism to understand it by doing.
+
+
+- How to declare API routes in a Flask app
+- How to get and set cookies
+- How to retrieve request form data
+- How to return various HTTP status codes
---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""SQLAlchemy User model, mapped to the users table"""
+
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import declarative_base
+
+
+Base = declarative_base()
+
+
+class User(Base):
+    """The mapping of the users table"""
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250))
+    reset_token = Column(String(250))
