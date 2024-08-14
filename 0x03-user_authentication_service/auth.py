3e7eb34a71fb853fc552e4118c9f06e865d69663
Yahia <yn7599@gmail.com>
2024-08-14 18:20:41 +0300

    session creation

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 """User Authentication Module"""
 
+from uuid import uuid4
+
 import bcrypt
-from sqlalchemy.exc import IntegrityError
+from sqlalchemy.exc import IntegrityError, NoResultFound
 
 from db import DB, User
 
@@ -10,6 +12,11 @@ from db import DB, User
 def _hash_password(password: str) -> bytes:
     """Hash a password using bcrypt"""
     return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+
+
+def _generate_uuid() -> str:
+    """Get a new UUID4 string"""
+    return str(uuid4())
 
 
 class Auth:
@@ -25,3 +32,20 @@ class Auth:
             return self._db.add_user(email, hashed_pw)
         except IntegrityError:
             raise ValueError(f"User {email} already exists")
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """Validate an email against a password"""
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            return False
+        return bcrypt.checkpw(password.encode(), user.hashed_password.encode())
+
+    def create_session(self, email: str) -> str:
+        """Create a new session and get the session ID"""
+        try:
+            user = self._db.find_user_by(email=email)
+            self._db.update_user(user.id, session_id=_generate_uuid())
+            return user.session_id
+        except Exception:
+            return None
