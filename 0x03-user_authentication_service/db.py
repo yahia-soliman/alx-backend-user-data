e8dbb2bb58743bed0c39f4019b5fd94ab73ae992
Yahia <yn7599@gmail.com>
2024-08-14 13:04:05 +0300

    TypeVar > User

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 """DataBase connection Wrapper"""
 
+from typing import TypeVar
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
@@ -26,7 +28,7 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email: str, hashed_password: str) -> User:
+    def add_user(self, email: str, hashed_password: str) -> TypeVar("User"):
         user = User(email=email, hashed_password=hashed_password)
         self._session.add(user)
         self._session.commit()
