cd40311b56b4f6015592b75da687919abeed12c8
Yahia <yn7599@gmail.com>
2024-08-15 00:53:36 +0300

    POST /reset_password

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -66,5 +66,16 @@ def profile():
         return abort(403)
 
 
+@app.route("/reset_password", methods=["POST"])
+def get_reset_password_token():
+    """Gett a reset password token"""
+    email = request.form.get("email")
+    try:
+        token = AUTH.get_reset_password_token(email)
+        return jsonify({"email": email, "reset_token": token})
+    except ValueError:
+        return abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
